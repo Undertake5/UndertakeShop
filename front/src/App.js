ce20ca1153dc385bb55d7915fb0a6800d8b7655f
1986sami <smmorales32@misena.edu.co>
2022-10-24 13:50:56 -0500

    Actualizacion de area principal

--- front/src/App.js
+++ front/src/App.js
@@ -1,11 +1,27 @@
 import './App.css';
+import React from 'react';
 
 
 function App() {
   return (
     <div className="App">
-      <h1>Undertake</h1>
-      
+        <img src="./imagenes/undertake.png" alt="logo" width="300" ></img>
+
+      <div class="bienvenida">
+        <h1>Bienvenido a la app de UNDERTAKE</h1>
+        <h2>Escoja su rol </h2>
+      </div>
+     
+      <div class="roles">
+                    <div> 
+                        <span>Administrador</span> 
+                        <a href="Administrador"> Administrador</a>  
+                    </div>
+                    <div>                 
+                        <span>cliente </span>           
+                        <a href="Cliente" >Cliente</a>
+                    </div>
+                </div>
     </div>
   );
 }
---
+++ front/src/componentes/admin/ListProduct.js
@@ -0,0 +1,2 @@
+import admin from component;
+
